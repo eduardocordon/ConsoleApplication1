fac9721b73387b02002d9d1704d3d66abbd3d685
eduardocordon <130512198+eduardocordon@users.noreply.github.com>
2023-05-11 10:34:14 -0700

    Update Particle.cpp

--- Code/Particle.cpp
+++ Code/Particle.cpp
@@ -12,9 +12,9 @@ Particle::Particle(RenderTarget& target, int numPoints, Vector2i mouseClickPosit
 
     m_centerCoordinate = target.mapPixelToCoords(mouseClickPosition, m_cartesianPlane);
 
-    m_vx = rand() % 1000 + 100;
+    m_vx = rand() % 400 + 100;
     if (rand() % 2 == 0) { m_vx *= -1; }
-    m_vy = rand() % 1000 + 100;
+    m_vy = rand() % 400 + 100;
 
     m_color1 = (Color::White);
     m_color2 = (Color(rand() % 255, rand() % 255, rand() % 255));
